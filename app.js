64a852602ac208198bcc3537506aff5f2c176fb2
Lucas8400 <lucas.burle@ynov.com>
2023-05-10 20:53:04 +0200

    fusion des fichiers filter.js et app.js et correction de certaines erreurs

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
+
+let articlesData = [];
+
 async function fetchArticles() {
-  try {
-    const response = await fetch('http://localhost:3000/articles');
-    const articles = await response.json();
-    displayArticles(articles);
-  } catch (error) {
-    console.error('Erreur lors de la récupération des articles :', error);
-  }
+    try {
+        const response = await fetch('http://localhost:3000/articles');
+        articlesData = await response.json();
+        displayArticles(articlesData);
+    } catch (error) {
+        console.error('Erreur lors de la récupération des articles :', error);
+    }
 }
 
 function displayArticles(articles) {
     const articleList = document.querySelector('.article-list');
+    articleList.innerHTML = '';
 
     articles.forEach(article => {
         const articlePreview = document.createElement('div');
@@ -60,7 +64,6 @@ function displayArticles(articles) {
             displayCart();
         });
 
-
         const viewDetailsBtn = document.createElement('button');
         viewDetailsBtn.textContent = 'Voir la fiche produit';
         viewDetailsBtn.addEventListener("click", () => {
@@ -73,9 +76,67 @@ function displayArticles(articles) {
 
         articlePreview.append(img, title, price, ctnButtons);
         articleList.appendChild(articlePreview);
-
     });
-
 }
 
+fetchArticles();
+
+function filterArticles(selectedColor, selectedCategory, selectedPrice) {
+    let filteredArticles = articlesData.filter(article => {
+        if (selectedColor === 'all' && selectedCategory === 'all') {
+            return true;
+        } else if (selectedColor === 'all' && selectedCategory !== 'all') {
+            return article.categorie === selectedCategory;
+        } else if (selectedColor !== 'all' && selectedCategory === 'all') {
+            return article.couleur === selectedColor;
+        } else {
+            return article.couleur === selectedColor && article.categorie === selectedCategory;
+        }
+    });
+
+    if (selectedPrice === 'price-asc') {
+        filteredArticles.sort((a, b) => a.prix - b.prix);
+    } else if (selectedPrice === 'price-desc') {
+        filteredArticles.sort((a, b) => b.prix - a.prix);
+    }
+
+    if (filteredArticles.length > 0) {
+        displayArticles(filteredArticles);
+    } else {
+        const articleList = document.querySelector('.article-list');
+        articleList.innerHTML = 'Aucun article trouvé';
+    }
+}
+
+const colorSelect = document.getElementById('color-select');
+colorSelect.addEventListener('change', () => {
+    const selectedColor = colorSelect.value;
+    const categorySelect = document.getElementById('category-select');
+    const selectedCategory = categorySelect.value;
+    const articleList = document.querySelector('.article-list');
+    articleList.innerHTML = '';
+    filterArticles(selectedColor, selectedCategory);
+});
+
+const categorySelect = document.getElementById('category-select');
+categorySelect.addEventListener('change', () => {
+    const selectedCategory = categorySelect.value;
+    const colorSelect = document.getElementById('color-select');
+    const selectedColor = colorSelect.value;
+    const articleList = document.querySelector('.article-list');
+    articleList.innerHTML = '';
+    filterArticles(selectedColor, selectedCategory);
+});
+
+const priceSelect = document.getElementById('price-select');
+priceSelect.addEventListener('change', () => {
+    const selectedPrice = priceSelect.value;
+    const colorSelect = document.getElementById('color-select');
+    const selectedColor = colorSelect.value;
+    const categorySelect = document.getElementById('category-select');
+    const selectedCategory = categorySelect.value;
+    const articleList = document.querySelector('.article-list');
+    articleList.innerHTML = '';
+    filterArticles(selectedColor, selectedCategory, selectedPrice);
+});
 
